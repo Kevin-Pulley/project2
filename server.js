cf057f9f6fa47ea5f8efd3ca4efaf0d4197de11b
Jonathan Stillman <jonathanstillman86@yahoo.com>
2020-11-09 21:34:53 -0500

    Update server.js
    Updating Server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+// Requiring Dependencies 
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+let db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+require("./routes/api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
