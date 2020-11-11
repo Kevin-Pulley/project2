a944a2c6300340fe8322c3700998edcd8b51bc32
Jonathan Stillman <jonathanstillman86@yahoo.com>
2020-11-10 22:14:48 -0500

    Re-align Info
    Re-align Info

--- db/getData.js
+++ db/getData.js
@@ -1,16 +1,16 @@
 var axios = require("axios").default;
 
 var options = {
-  method: 'GET',
-  url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/null',
-  headers: {
-    'x-rapidapi-key': '8014185b2fmshfeeb5c4c4378010p1d437ejsnfd6e7a37960f',
-    'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
-  }
+    method: 'GET',
+    url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/null',
+    headers: {
+        'x-rapidapi-key': '8014185b2fmshfeeb5c4c4378010p1d437ejsnfd6e7a37960f',
+        'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+    }
 };
 
 axios.request(options).then(function (response) {
-	console.log(response.data);
+    console.log(response.data);
 }).catch(function (error) {
-	console.error(error);
+    console.error(error);
 });
