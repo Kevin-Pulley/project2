33b933ca5c26525f75b205f9eed75538ed79043a
arrow913 <billy.arrasmith@gmail.com>
2020-11-11 19:58:46 -0600

    added to Routes

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 8080;
 let db = require("./models");
 
 // Set Handlebars as the default templating engine.
+
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -21,9 +23,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Routes
-require("./routes/cards.js")(app);
-require("./routes/class.js")(app);
-require("./routes/deck.js")(app);
+let cardsRoutes = require("./routes/cards.js");
+app.use(cardsRoutes);
+let classRoutes = require("./routes/class.js");
+app.use(classRoutes);
+let decksRoutes = require("./routes/deck.js");
+app.use(decksRoutes);
 
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync().then(function() {
