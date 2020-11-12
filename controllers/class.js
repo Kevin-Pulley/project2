af64f570b62171fe79581b7fe5572a2bc3f02881
hilekat <68864060+hilekat@users.noreply.github.com>
2020-11-11 20:14:50 -0600

    Update class.js

--- controllers/class.js
+++ controllers/class.js
@@ -2,28 +2,25 @@ const Class = require("../models/class.js");
 
 let classController = {
     index: (req, res) => {
-        res.json(req.params.name);
+        res.json(req.params.class);
     },
     showClass: (req, res) => {
-        res.json(req.params.name);
+        res.json(req.params.class);
     },
     getById: (req, res) => {
-        res.json(req.params.name);
+        res.json(req.params.class);
     },
     getByName: (req, res) => {
-        res.json(req.params.name);
-    },
-    classSearch: (req, res) => {
-        res.json(req.params.name);
+        res.json(req.params.class);
     },
     create: (req, res) => {
-        res.json(req.params.name);
+        res.json(req.params.class);
     },
     edit: (req, res) => {
-        res.json(req.params.name);
+        res.json(req.params.class);
     },
     delete: (req, res) => {
-        res.json(req.params.name);
+        res.json(req.params.class);
     },
 }
 
