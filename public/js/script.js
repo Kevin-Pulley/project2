2cd7ec8543e45154328cc573719e66918818eca0
Kevin Pulley <skippy1025@gmail.com>
2020-11-12 20:50:48 -0600

    worked on the ajax calls, adde index.handlebars, it is now our home page"

---
+++ public/js/script.js
@@ -0,0 +1,31 @@
+$("#searchBtn").on("click", function () {
+  const searchValue = $("#search-input").val();
+  console.log(searchValue);
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${searchValue}`,
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "e25253654bmshbe1d4ddbcd23335p1dc5a9jsndabf8328bd41",
+      "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    },
+  };
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    $("#card-container").html(`<img src="${response[0].img}"/>`);
+  });
+});
+
+$("#card-container").on("click", "img", function () {
+  const img = $(this).attr("src");
+  console.log(img);
+  const newCard = {
+    image_url: img,
+    name: "test",
+    api_url: "test"
+  };
+
+  $.post("/api/card", newCard);
+});
--- routes/cards.js
+++ routes/cards.js
@@ -4,11 +4,11 @@ const cardController = require("../controllers/card.js");
 
 router.get("/api/cards/:name", cardController.showCard);
 router.get("/", cardController.index);
-router.get("/id/:id", cardController.getById);
-router.get("/name/:name", cardController.getByName);
-router.post("/", cardController.create);
+router.get("/api/id/:id", cardController.getById);
+router.get("/api/name/:name", cardController.getByName);
+router.post("/api/card", cardController.create);
 router.put("/:name", cardController.edit);
 router.delete("/:name", cardController.delete);
-router.post("/cardsearch", cardController.cardSearch);
+router.post("/api/cards/cardsearch", cardController.cardSearch);
 
 module.exports = router;
