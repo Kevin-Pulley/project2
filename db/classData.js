f31a27b61fbe2d220e09588782285088092ac24a
Kevin Pulley <skippy1025@gmail.com>
2020-11-14 10:01:26 -0600

    Changes
    Got buttons pulling up on index.handlebars

---
+++ db/classData.js
@@ -0,0 +1,39 @@
+
+module.exports = {
+
+classes:
+[
+    {
+      "name": "Priest"
+    },
+    {
+      "name": "Warrior"
+    },
+    {
+      "name": "Warlock"
+    },
+    {
+      "name": "Mage"
+    },
+    {
+      "name": "Druid"
+    },
+    {
+      "name": "Hunter"
+    },
+    {
+      "name": "Shaman"
+    },
+    {
+      "name": "Paladin"
+    },
+    {
+      "name": "Rogue"
+    },
+    {
+      "name": "Neutral"
+    }
+  ]
+}
+
+  
